462788ff6425a08010e1be41461177d06e20bb2c
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-09-05 12:54:21 +0200

    Initial commit

---
+++ app/Models/Item.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    /** @use HasFactory<\Database\Factories\ItemFactory> */
+    use HasFactory;
+    public function author()
+    {
+        return $this->belongsTo(User::class, 'author_id');
+    }
+}
