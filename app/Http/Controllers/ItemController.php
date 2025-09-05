2a797454d7f4fe9b2315188b5ef0a927cc55e799
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-09-05 17:45:31 +0200

    Feature🎉ItemController set-up

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -6,5 +6,15 @@ use Illuminate\Http\Request;
 
 class ItemController extends Controller
 {
-    //
+    public function index()
+    {
+        // fetch articles from DB
+        $items = \App\Models\Item::all();
+
+        //dd($articles); // to quickly analyse what you loaded
+
+        // send articles to the view
+        // return response
+        return view('item.index', compact('items'));
+    }
 }
