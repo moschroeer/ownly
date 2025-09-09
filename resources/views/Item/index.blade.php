91278dfb17cda951241c7a927f99fa5b5ffb7361
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-09-09 11:34:54 +0200

    Feature 🎉 Routing item to view

--- resources/views/Item/index.blade.php
+++ resources/views/Item/index.blade.php
@@ -1,1 +1,11 @@
-<?php
+
+
+<html>
+    <head>
+        <title>Item overview</title>
+        <script src="https://cdn.jsdelivr.net/npm/@tailwindcss/browser@4"></script>
+    </head>
+    <body>
+        hello world
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,8 @@ use App\Livewire\Settings\Password;
 use App\Livewire\Settings\Profile;
 use Illuminate\Support\Facades\Route;
 
+Route::get('items', [\App\Http\Controllers\ItemController::class, 'index'] );
+
 Route::get('/', function () {
     return view('welcome');
 })->name('home');
