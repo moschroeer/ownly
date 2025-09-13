30b13435052b8a20c34aa781e978a4d585d34898
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-09-13 16:19:34 +0200

    Design 🎨 Item index view

--- resources/views/Item/index.blade.php
+++ resources/views/Item/index.blade.php
@@ -2,10 +2,15 @@
 
     <h1 class="text-4xl font-bold">Items overview</h1>
 
+    <div class="mx-auto max-w-7xl py-6">
+        <div class="hidden md:grid md:grid-cols-4 md:gap-6">
     @foreach($items as $item)
-        <div>
-            <a href="/items/{{$item->id}}">{{ $item->title }}</a>
+        <div class="rounded-2xl bg-white shadow-sm ring-1 ring-slate-200 p-6">
+            <div class="w-full h-40 rounded-xl bg-slate-200 mb-2" ></div>
+            <a href="/items/{{$item->id}}" class="text-lg font-semibold mt-6">{{ $item->title }}</a>
+            <div>{{$item->category}}</div>
         </div>
     @endforeach
-
+        </div>
+    </div>
 </x-site-layout>
