c290cba82db7293d04de23b1848e5df28c46be8c
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-10-04 19:05:36 +0200

    Feature 🎉Seed admin user

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,12 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
+        \App\Models\User::create([
+            'name'=> 'admin',
+            'email'=> 'admin@admin.com',
+            'password'=> Hash::make('password'),
+            ]);
+
         \App\Models\User::factory(10)->create();
         \App\Models\Item::factory(15)->create();
         \App\Models\Reservation::factory(5)->create();
