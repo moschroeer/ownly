f0dfd9757792330cc809d72a97295872cb327e01
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-10-05 21:13:44 +0200

    Design 🎨User index + show view

--- resources/views/User/index.blade.php
+++ resources/views/User/index.blade.php
@@ -1,11 +1,15 @@
 <x-site-layout>
 
     <h1 class="text-4xl font-bold">Users</h1>
+    <div class="mx-auto max-w-7xl py-6">
+        <div class="place-self-start hidden md:grid md:grid-cols-4 md:gap-6">
+            @foreach($users as $user)
+                <div class="rounded-2xl bg-white shadow-lg ring-2 ring-white p-6 w-56">
+                    <div class="w-40 h-40 rounded-full bg-slate-200 mb-2 place-self-center" ></div>
+                    <div class="place-self-center"><a href="/users/{{$user->id}}" class="text-lg font-semibold mt-6">{{ $user->name}}</a></div>
+                 </div>
+            @endforeach
 
-    @foreach($users as $user)
-        <div>
-            <a href="/users/{{$user->id}}">{{ $user->name }}</a>
         </div>
-    @endforeach
-
+    </div>
 </x-site-layout>
--- resources/views/User/show.blade.php
+++ resources/views/User/show.blade.php
@@ -1,12 +1,17 @@
 
 <x-site-layout>
-
+    <section class="mx-auto w-3/5 py-6 rounded-2xl bg-white ring-2 ring-white bg-white shadow-lg p-6 my-6">
     <h1 class="text-4xl font-bold">{{$users->name}}</h1>
 
     <div class="mb-2 text-blue-800">List of shared Items:</div>
-
+        <div class="hidden md:grid md:grid-cols-2 md:gap-6">
     @foreach($users->items as $item)
-        {{ $item->title }}<br/>
+            <div class=" rounded-2xl bg-white shadow-lg ring-2 ring-white p-6">
+                <div class="w-full h-40 rounded-xl bg-slate-200 mb-2" ></div>
+                <a href="/items/{{$item->id}}" class="text-lg font-semibold mt-6">{{ $item->title }}</a>
+                <div>{{$item->category}}</div>
+            </div>
     @endforeach
-
+        </div>
+    </section>
 </x-site-layout>
