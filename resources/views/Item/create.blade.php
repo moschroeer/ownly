070fe58dcd3e03f12367200c663a6267f4d8d071
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-10-05 21:11:07 +0200

    Refactor 🔨 Missing verification for input on Create Page

--- resources/views/Item/create.blade.php
+++ resources/views/Item/create.blade.php
@@ -10,11 +10,17 @@
             <input type="text" name="title"
                    class="bg-slate-100 p-2 rounded-md w-full mt-1"
             />
+            @error('title')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
 
         <div class="max-w-md my-4">
             <label for="description"> Description:</label><br/>
             <textarea name="description" class="bg-slate-100 p-2 rounded-md w-full min-h-40 mt-1">Describe your item</textarea>
+            @error('description')
+            <p class="text-red-500 text-xs mt-1">{{$message}}</p>
+            @enderror
         </div>
     <button class="inline-flex rounded-md px-4 py-2 border-2 border-blue-500 hover:border-blue-700 bg-blue-500 hover:bg-blue-700 text-white" type="submit"> Create </button>
     </form>
