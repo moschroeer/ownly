70f17b401c24d48f9ec6dc7ea53177989f526d9f
Moritz Schroeer <moritz.schroeer@code.berlin>
2025-10-11 18:57:14 +0200

    Design 🎨Layout for the Items show view

--- resources/views/Item/show.blade.php
+++ resources/views/Item/show.blade.php
@@ -1,5 +1,5 @@
 <x-site-layout>
-    <div class="mx-auto w-3/5 py-6 rounded-2xl bg-white shadow-lg ring-2 ring-white p-6 my-6">
+    <div class="mx-auto max-w-2xl my-6 rounded-2xl bg-white shadow-lg ring-2 ring-white p-6">
     <section class="flex items-center justify-between gap-4">
         <div>
             <h1 class="text-4xl font-bold">{{$items->title}}</h1>
@@ -24,9 +24,17 @@
             @endif
         @endauth
     </section>
-
-
-    <div class="mb-2 text-blue-800">shared by: {{$items->owner->name}}</div>
+<div class="flex flex-nowrap items-center gap-3 mt-2">
+        <div class="h-8 inline-flex items-center rounded-md px-3 text-sm font-medium ring-1 bg-gray-50 text-gray-600 ring-gray-200"> {{$items->category}} </div>
+        <div class="h-8 inline-flex items-center rounded-md px-3 text-sm font-medium ring-1 bg-blue-50 text-blue-500 ring-blue-200"><x-hugeicons-location-01 class="w-6 h-6 mr-2"/> {{$items->address}} </div>
+</div>
+    <section class="grid grid-cols-3 gap-4 grid-rows-2 h-80 my-4">
+            <div class="col-span-2 row-span-2 rounded-xl bg-slate-200" ></div>
+                <div class="rounded-xl bg-slate-200" ></div>
+                <div class="rounded-xl bg-slate-200" ></div>
+    </section>
+    <div class="mb-2 text-blue-800">Shared by: <a href="/users/{{$items->owner->id}}" class="hover:underline underline-offset-2">{{$items->owner->name}}</a>
+        </div>
 
     <h1 class="text-2xl font-semibold">Description</h1>
     <div>
